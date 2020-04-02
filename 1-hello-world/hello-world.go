90ba9677388ef449337ff52584ac8c8574d652cb
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-04-01 22:39:07 -0300

    Hello world

---
+++ 1-hello-world/hello-world.go
@@ -0,0 +1,17 @@
+package main
+
+// Nosso primeiro programa irá "printar" a clássica mensagem de "hello world".
+// Abaixo segue o código fonte completo.
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// Para rodar o programa, basta utilizarmos o comando "go run" no diretório atual.
+
+// Alguma vezes nós poderemos querer "buildar" nosso programa em binários. Nós podemos
+// fazer isso utilizando o comando "go build".
+
+// Dessa forma, nós poderemos executar o binário "buildado" diretamente.
